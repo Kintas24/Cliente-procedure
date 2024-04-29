6414b6b263d9f06ee84aa14dd57a6ead1a6b64fc
Jorge Kintas <jorgekintas.jk@outlook.com>
2024-04-29 21:04:30 +0100

    .

---
+++ clienteprocedure.sql
@@ -0,0 +1,10 @@
+CREATE PROCEDURE relatorio_quantidade_produtos_vendidos_por_dia()
+BEGIN
+    SELECT 
+        data_venda,
+        SUM(quantidade) AS total_produtos_vendidos
+    FROM 
+        vendas
+    GROUP BY 
+        data_venda;
+END
